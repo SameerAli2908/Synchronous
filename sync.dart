5fe33116eef37eb6832ccdd7c18416116dff9968
Sameer Khan <sk5749857@gmail.com>
2026-04-06 05:42:04 -0700

    Add files via upload

---
+++ sync.dart
@@ -0,0 +1,15 @@
+// ------------Synchronous------------
+//             Yeh Wait Karta Hai
+//             Slow Hai
+//             Simple Task Perform Karta Hai
+//             Example : Atm MAchine , Attendance MAchine
+void main() {
+
+print ("Step 1 Pakistan");
+print ("Step 2 Karachi");
+print ("Step 3 Checkin in Hotel");
+print ("Step 4 Seaview");
+print ("Step 5 Checkout in Hotel");
+print ("Step 6 Airport");
+print ("Step 7 Home Country");
+}
